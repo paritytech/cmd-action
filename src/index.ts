94dcab0d77cbbed1e68de90012851b1f9b5e8359
Javier Bullrich <javier@bullrich.dev>
2024-03-22 13:01:58 +0100

    broke the code styling

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { context, getOctokit } from "@actions/github";
 import { Context } from "@actions/github/lib/context";
 import { PullRequest } from "@octokit/webhooks-types";
 
-import { PullRequestApi } from "./github/pullRequest";
+import { PullRequestApi } from "./github/pullRequest"
 import { generateCoreLogger } from "./util";
 
 const getRepo = (ctx: Context) => {
