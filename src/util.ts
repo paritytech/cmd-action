8e20ceb472734ecb3a7a20b9a941b0bd2689e2a0
Javier Bullrich <javier@bullrich.dev>
2024-04-17 12:02:49 +0200

    added utility function to find files

--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,27 @@
 import { debug, error, info, warning } from "@actions/core";
+import fs from "fs/promises";
+import { resolve } from "path";
 
 import { ActionLogger } from "./github/types";
 
 export function generateCoreLogger(): ActionLogger {
   return { info, debug, warn: warning, error };
+}
+
+export async function findFilesWithExtension(
+  dir: string,
+  ext: string,
+): Promise<string[]> {
+  const files: string[] = [];
+  const dirents = await fs.readdir(dir, { withFileTypes: true });
+  for (const dirent of dirents) {
+    const res = resolve(dir, dirent.name);
+    if (dirent.isDirectory()) {
+      files.push(...(await findFilesWithExtension(res, ext)));
+    } else if (res.endsWith(ext)) {
+      files.push(res);
+    }
+  }
+
+  return files;
 }
