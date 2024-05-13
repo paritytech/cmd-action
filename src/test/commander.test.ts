3e3533966412a965369249e74827f9cd157643eb
Javier Bullrich <javier@bullrich.dev>
2024-05-13 17:12:34 +0200

    added test to evaluate fetching commands

---
+++ src/test/commander.test.ts
@@ -0,0 +1,9 @@
+import { Commander } from "../commander";
+import { generateCoreLogger } from "../util";
+
+
+test("test command inside repository", async () => {
+    const commander = new Commander(".github/scripts", generateCoreLogger());
+    const commands = await commander.getCommands();
+    expect(commands.length).toBeGreaterThan(0);
+});
